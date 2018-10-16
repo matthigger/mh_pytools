294eb8a03043a8afcf3bd21c1b802b6529bdeb0a
matt higger <matt.higger@gmail.com>
2018-10-16 08:53:58 -0400

    rename - paralell -> parallel - eventually I'll learn to spel
    add - run_par_fnc() - now accepts obj

--- mh_pytools/parallel.py
+++ mh_pytools/parallel.py
@@ -24,9 +24,13 @@ def proxy_fnc(dict_in):
     >>> proxy_fnc(fnc=add_one, x=1)
     2
     """
-    # get fnc, rm from dict_in
-    fnc = dict_in['fnc']
-    del dict_in['fnc']
+    # get fnc, obj and rm from dict_in
+    fnc = dict_in.pop('fnc')
+    obj = dict_in.pop('obj')
+
+    # if obj is passed, fnc is name of its method to be run
+    if obj is not None:
+        fnc = getattr(obj, fnc)
 
     # apply fnc to unpacked dict_in
     return fnc(**dict_in)
@@ -75,27 +79,35 @@ def join(pool, res, check_sec=1, verbose=True, desc='task', garbage=True):
     return res.get()
 
 
-def run_par_fnc(fnc, arg_list, desc=None, **kwargs):
+def run_par_fnc(fnc, arg_list, obj=None, desc=None, **kwargs):
     """ runs a function in parallel
 
     Args:
-        fnc (fnc or str): function to be run in paralell, or name of method to
-                          be run in parallel
+        fnc (fnc or str): fnc or name of obj's method to run
         arg_list (list): list of dicts, to be unpacked in running fnc
+        obj: if passed, fnc is a str and obj defines obj whose method is run
         desc (str): description of operation
 
     Returns:
         res_list (list): list of results, same order as arg_list
 
-    >>> run_par_fnc(add_one, [{'x': x} for x in range(10)], verbose=False)
+    >>> arg_list = [{'x': x} for x in range(10)]
+    >>> run_par_fnc(add_one, arg_list, verbose=False)
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    >>> obj = AddState(10)
+    >>> run_par_fnc(fnc='add_state', arg_list=arg_list, obj=obj, verbose=False)
+    [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
     """
-
-    if desc is None:
-        desc = fnc.__name__
 
     for d in arg_list:
         d['fnc'] = fnc
+        d['obj'] = obj
+
+    if desc is None:
+        if obj is None:
+            desc = fnc.__name__
+        else:
+            desc = f'{fnc} of {obj}'
 
     pool = multiprocessing.Pool()
     res = pool.map_async(proxy_fnc, arg_list)
